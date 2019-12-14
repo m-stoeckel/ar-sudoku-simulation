223c9b1f547922d262ed3422afc9beca177c5041
Manuel Stoeckel <s3676959@stud.uni-frankfurt.de>
2019-12-14 09:12:53 +0100

    Added major switch transform
    - Changed the way transforms work, they no longer return the unchanged sudoku as a first item in their return list.

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from PIL import Image
 
-from sudoku_generator import SudokuGenerator, Sudoku
-from transform import TransformSudokuGenerator, Rotation
+from transform import TransformSudokuGenerator, MajorSwitch
 
 
 def save_img(img: Image, name: str):
@@ -10,31 +9,35 @@ def save_img(img: Image, name: str):
 
 
 def temp_row_transform():
-    sgen = SudokuGenerator(1, workers=8)
-    print(sgen.generated[0].data.copy())
+    sgen = TransformSudokuGenerator(1, workers=8)
+    sgen.transforms.append(MajorSwitch(row_switch=(1, 0, 2)))
+    sgen.transforms.append(MajorSwitch(column_switch=(1, 0, 2)))
+    sgen.transforms.append(MajorSwitch(row_switch=(1, 0, 2), column_switch=(1, 0, 2)))
+    sgen.build()
+    print(sgen.generated[0].data)
     save_img(sgen.draw_sudoku_pil(0, masking_rate=0.0), f"sudoku_0.0_switchM([0,1,2],[0,1,2]).png")
-    sudoku = Sudoku.from_array(sgen.generated[0].data.copy())
-    sudoku.data[:, [3, 4, 5, 0, 1, 2, 6, 7, 8]] = sgen.generated[0].data[:, [0, 1, 2, 3, 4, 5, 6, 7, 8]]
-    sgen.generated.append(sudoku)
+
+    print(sgen.generated[1].data)
+    print(f"Is valid: {sgen.generated[1].is_valid:b}")
     save_img(sgen.draw_sudoku_pil(1, masking_rate=0.0), f"sudoku_0.0_switchM([0,1,2],[1,0,2]).png")
-    print(sudoku.data)
-    print(f"Is valid: {sudoku.is_valid:b}")
-    sudoku = Sudoku.from_array(sgen.generated[0].data.copy())
-    sudoku.data[[3, 4, 5, 0, 1, 2, 6, 7, 8]] = sgen.generated[1].data[[0, 1, 2, 3, 4, 5, 6, 7, 8]]
-    sgen.generated.append(sudoku)
+
+    print(sgen.generated[2].data)
+    print(f"Is valid: {sgen.generated[2].is_valid:b}")
     save_img(sgen.draw_sudoku_pil(2, masking_rate=0.0), f"sudoku_0.0_switchM([1,0,2],[0,1,2]).png")
-    print(sudoku.data)
-    print(f"Is valid: {sudoku.is_valid:b}")
+
+    print(sgen.generated[3].data)
+    print(f"Is valid: {sgen.generated[3].is_valid:b}")
+    save_img(sgen.draw_sudoku_pil(3, masking_rate=0.0), f"sudoku_0.0_switchM([1,0,2],[1,0,2]).png")
 
 
 if __name__ == '__main__':
     # mnist = MNIST()
-    sgen = TransformSudokuGenerator(1, workers=8)
-    sgen.add_transform(Rotation())
-    sgen.build()
-    print(sgen.generated[2].data)
+    # sgen = TransformSudokuGenerator(1, workers=8)
+    # sgen.add_transform(Rotation())
+    # sgen.build()
+    # print(sgen.generated[2].data)
 
-    # temp_row_transform()
+    temp_row_transform()
     # sgen.draw_sudoku_pil(0)
     # for p in [0., .2, .4, .6, .8, 1.0]:
     #     save_img(sgen.draw_sudoku_pil_mnist(0, mnist=mnist, mnist_rate=p), f"sudoku_0.7_{p}.png")
--- transform.py
+++ transform.py
@@ -12,7 +12,7 @@ class Transform:
 
 class Rotation(Transform):
     def apply(self, sudoku: Sudoku) -> List[Sudoku]:
-        ret: List[Sudoku] = [sudoku]
+        ret: List[Sudoku] = []
         for i in range(3):
             sudoku_from_array = Sudoku.from_array(np.rot90(ret[i].data))
             if sudoku_from_array.is_valid:
@@ -22,13 +22,38 @@ class Rotation(Transform):
 
 class Flip(Transform):
     def apply(self, sudoku: Sudoku) -> List[Sudoku]:
-        ret: List[Sudoku] = [sudoku]
+        ret: List[Sudoku] = []
 
         for ax in [0, 1, None]:
             sudoku_from_array = Sudoku.from_array(np.flip(sudoku.data, ax))
             if sudoku_from_array.is_valid:
                 ret.append(sudoku_from_array)
-        return ret
+            return ret
+
+
+class MajorSwitch(Transform):
+    def __init__(self, column_switch=(0, 1, 2), row_switch=(0, 1, 2), column_first=False):
+        lookup = [[0, 1, 2], [3, 4, 5], [6, 7, 8]]
+        if not set(column_switch) == set(row_switch) == {0, 1, 2}:
+            raise ValueError()
+
+        self.column_order = [n for cid in column_switch for n in lookup[cid]]
+        self.row_order = [n for cid in row_switch for n in lookup[cid]]
+        self.column_first = column_first
+
+    def apply(self, sudoku: Sudoku) -> List[Sudoku]:
+        sudoku_from_array = Sudoku.from_array(sudoku.data.copy())
+        if self.column_first:
+            sudoku_from_array.data[:, self.column_order] = sudoku.data[:, [0, 1, 2, 3, 4, 5, 6, 7, 8]]
+            sudoku_from_array.data[self.row_order, :] = sudoku_from_array.data[[0, 1, 2, 3, 4, 5, 6, 7, 8], :]
+        else:
+            sudoku_from_array.data[self.row_order, :] = sudoku.data[[0, 1, 2, 3, 4, 5, 6, 7, 8], :]
+            sudoku_from_array.data[:, self.column_order] = sudoku_from_array.data[:, [0, 1, 2, 3, 4, 5, 6, 7, 8]]
+
+        if not sudoku_from_array.is_valid:
+            print(sudoku_from_array.data)
+            raise ValueError
+        return [sudoku_from_array]
 
 
 class TransformSudokuGenerator(SudokuGenerator):
@@ -44,4 +69,4 @@ class TransformSudokuGenerator(SudokuGenerator):
             newl: List[Sudoku] = []
             for s in self.generated:
                 newl.extend(transform.apply(s))
-            self.generated = newl
+            self.generated.extend(newl)
