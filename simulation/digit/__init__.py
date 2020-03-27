bcd1062255df45b9d7b9d71737b3612b2ce06293
Manuel Stoeckel <manuel.stoeckel@stud.uni-frankfurt.de>
2020-03-27 11:40:24 +0100

    Added RealDataset

--- simulation/digit/__init__.py
+++ simulation/digit/__init__.py
@@ -1,3 +1,3 @@
-from .character_provider import CharacterRenderer, SingleFontCharacterRenderer
-from .data_generator import DigitDataGenerator, BalancedDataGenerator
+from .character_provider import *
+from .data_generator import *
 from .dataset import *
